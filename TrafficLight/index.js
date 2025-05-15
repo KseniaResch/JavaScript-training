6aa8a5795c158843d88598347aa6ac92ca244a7d
KseniaResch <kseniaresch@gmail.com>
2025-05-15 21:57:07 +0200

    countdown added

--- TrafficLight/index.js
+++ TrafficLight/index.js
@@ -4,6 +4,45 @@ console.dir(redlight)
 
 redlight.style.opacity = 1;
 redlight.style.opacity= 0.5;*/
+let timer = document.getElementById("timer");
+let countdownGO = 3;
+let countdownSTOP = 3;
+let countdownGoInterval;
+let countdownStopInterval;
+
+
+const countdownGo = () =>{
+    timer.textContent = countdownGO;
+    countdownGO--;
+        if (countdownGO < 0) {
+        clearInterval(countdownGoInterval);
+        timer.textContent = "GO!"
+        
+    }
+}
+
+const countdownStop = () =>{
+    timer.textContent = countdownSTOP;
+    countdownSTOP--;
+        if (countdownSTOP < 0) {
+        clearInterval(countdownStopInterval);
+        timer.textContent = "STOP!"
+        
+    }
+}
+
+const beginCountdownGO = () => {
+  countdownGO = 3;
+  timer.textContent = countdownGO;
+  countdownGoInterval = setInterval(countdownGo, 1000); 
+};
+
+const beginCountdownStop = () => {
+  countdownSTOP = 3;
+  timer.textContent = countdownSTOP;
+  countdownStopInterval = setInterval(countdownStop, 1000); 
+};
+
 
 const switchOn = (id) => {
     let light = document.getElementById(id);
@@ -21,6 +60,8 @@ let yellowAfterRed = 7000;
 let greenAfterYellow = 10000;
 let yellowAfterGreen = 17000;
 let switchOffGreen = 20000;
+let timerStartGo = 6000;
+let timerStartStop= 16000;
 
 const start = () => {
     switchOn("red");
@@ -28,14 +69,21 @@ const start = () => {
     setTimeout(() => {
      switchOn("yellow");   
     }, yellowAfterRed);
+
+    setTimeout(() => {
+     beginCountdownGO()   
+    }, timerStartGo);
     
 
     setTimeout(() => {
         switchOff("red");
         switchOff("yellow");
         switchOn("green");
-
     }, greenAfterYellow);
+
+    setTimeout(() => {
+     beginCountdownStop()   
+    }, timerStartStop);
 
      setTimeout(() => {
      switchOn("yellow");   
@@ -47,6 +95,8 @@ const start = () => {
     }, switchOffGreen);
 
 }
+
+
 
 
 start();
