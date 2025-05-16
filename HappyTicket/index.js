dc5f2910e322a805539c5d4421cd46999cbe8195
KseniaResch <kseniaresch@gmail.com>
2025-05-16 20:52:49 +0200

    HappyTicket created

--- HappyTicket/index.js
+++ HappyTicket/index.js
@@ -1,18 +1,18 @@
 
 
-const checkTicket = ((ticketNumbers)=> {
-     const ticketNumbers = [];
+const checkTicket = ((ticketString)=> { 
+  if (ticketString.length != 6 ) {
+        return " get another ticket"
+      }
+      
+      const ticketNumbers = ticketString.split("").map(Number);
      sum1 = ticketNumbers[0] + ticketNumbers[1] + ticketNumbers [2];
      sum2 = ticketNumbers[3] + ticketNumbers[4] + ticketNumbers [5];
-      
-     if (ticketNumbers.length != 6 ) {
-        return " get another ticket"
-    } else if (sum1 = sum2){
-      return "happy"
-    } else { 
+       if (sum1 === sum2){
+        return "happy"
+      } else { 
         return "not happy"
-
-    }     
+       }     
 
 });
 
@@ -20,3 +20,4 @@ const checkTicket = ((ticketNumbers)=> {
 console.log(checkTicket('005212')); // true
 console.log(checkTicket('133700')); // true
 console.log(checkTicket('123032')); // false
+console.log(checkTicket('1230329892802')); // false
