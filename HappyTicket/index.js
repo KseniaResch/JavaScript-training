a49a15917f332c066367078f54c0f84d018f7873
KseniaResch <kseniaresch@gmail.com>
2025-05-20 11:06:16 +0200

    in progress

--- HappyTicket/index.js
+++ HappyTicket/index.js
@@ -1,14 +1,30 @@
 
 
 const checkTicket = ((ticketString)=> { 
-  if (ticketString.length != 6 ) {
-        return " get another ticket"
-      }
+ if (ticketString.length % 2 !== 0 ) {
+        return "get another ticket"}
+
+      const half = ticketString.length / 2;
+      const firstHalf  =  ticketString.slice(0, half);
+      const secondHalf = ticketString.slice(half)
       
-      const ticketNumbers = ticketString.split("").map(Number);
-     sum1 = ticketNumbers[0] + ticketNumbers[1] + ticketNumbers [2];
-     sum2 = ticketNumbers[3] + ticketNumbers[4] + ticketNumbers [5];
-       if (sum1 === sum2){
+      const firstHalftNumbers = firstHalf.split("").map(Number);
+      const secondHalfNumber = secondHalf.split("").map(Number);
+
+    const firstHalfArray= firstHalftNumbers;
+    const  secondHalfArray= secondHalfNumber;
+    const sumOfNumbers1 = 0;
+    const  sumOfNumbers2 = 0;
+
+      for (let i =  0; i < firstHalfArray.length; i++){
+      sumOfNumbers1 += firstHalfArray[i];
+    }
+
+      for ( let i = 0; i < secondHalfArray.length; i++){
+      sumOfNumbers2 += secondHalfArray[i];
+      }
+
+       if (sumOfNumbers1 === sumOfNumbers2){
         return "happy"
       } else { 
         return "not happy"
@@ -21,3 +37,4 @@ console.log(checkTicket('005212')); // true
 console.log(checkTicket('133700')); // true
 console.log(checkTicket('123032')); // false
 console.log(checkTicket('1230329892802')); // false
+console.log(checkTicket('831948195418'));
