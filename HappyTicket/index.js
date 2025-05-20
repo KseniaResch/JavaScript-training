058d06f3f2270a4656aa0d251a70f0e5e6342845
KseniaResch <kseniaresch@gmail.com>
2025-05-20 19:12:45 +0200

    reduce added

--- HappyTicket/index.js
+++ HappyTicket/index.js
@@ -8,21 +8,53 @@ const checkTicket = ((ticketString)=> {
       const firstHalf  =  ticketString.slice(0, half);
       const secondHalf = ticketString.slice(half)
       
-      const firstHalftNumbers = firstHalf.split("").map(Number);
-      const secondHalfNumber = secondHalf.split("").map(Number);
+       const firstHalftNumbers = firstHalf.split("").map(Number);
+       const secondHalfNumber = secondHalf.split("").map(Number);
+
 
     const firstHalfArray= firstHalftNumbers;
     const  secondHalfArray= secondHalfNumber;
     let sumOfNumbers1 = 0;
     let  sumOfNumbers2 = 0;
 
-      for (let i =  0; i < firstHalfArray.length; i++){
-      sumOfNumbers1 += firstHalfArray[i];
-    }
+    console.log(11111, firstHalfArray);
+    
 
-      for ( let i = 0; i < secondHalfArray.length; i++){
-      sumOfNumbers2 += secondHalfArray[i];
-      }
+   sumOfNumbers1 = firstHalfArray.reduce((accum, number, array) => {
+      accum += number;
+       
+      return accum;
+    }, 10)
+
+    sumOfNumbers2 = secondHalfArray.reduce((accum, number, array) => {
+      accum += number;
+       
+      return accum;
+    }, 10)
+
+
+
+// sumOfNumbers1 = [{Worker: "Sam", Salary: 125}, {Worker: "Bob", Salary: 160}, {Worker: "Tom", Salary: 180}].reduce((accum, user, array) => {
+//       console.log("accum", accum);
+//       console.log("user", user);
+      
+      
+//       accum += user.Salary;
+       
+//       return accum;
+      
+
+//     }, 0)  
+
+    
+
+    //   for (let i =  0; i < firstHalfArray.length; i++){
+    //   sumOfNumbers1 += firstHalfArray[i];
+    // }
+
+    //   for ( let i = 0; i < secondHalfArray.length; i++){
+    //   sumOfNumbers2 += secondHalfArray[i];
+    //   }
 
        if (sumOfNumbers1 === sumOfNumbers2){
         return "happy"
@@ -34,7 +66,7 @@ const checkTicket = ((ticketString)=> {
 
 
 console.log(checkTicket('005212')); // true
-console.log(checkTicket('133700')); // true
-console.log(checkTicket('123032')); // false
-console.log(checkTicket('1230329892802')); // false
-console.log(checkTicket('831948195418'));
+ console.log(checkTicket('133700')); // true
+ console.log(checkTicket('123032')); // false
+ console.log(checkTicket('1230329892802')); // false
+ console.log(checkTicket('831948195418'));
--- Palindrom/index.js
+++ Palindrom/index.js
@@ -1,14 +1,15 @@
-const isPalindrome = ((str) => {
+const isPalindrome = (str) => {
    let readForewards = str;
    let readBackwards = readForewards.split("").reverse().join("");
 
-    if (readForewards === readBackwards) {
-        return true
-    } else { 
-        return false
-    }
+    // if (readForewards === readBackwards) {
+    //     return true
+    // } else { 
+    //     return false
+    // }
+    return readBackwards === readForewards;
     
-})
+}
 
 console.log(isPalindrome('121')); // true
 console.log(isPalindrome('boob')); // true
@@ -18,19 +19,20 @@ console.log(isPalindrome('true')); // false
 
 //second variant
 
-const checkPalindrome = ((str) => {
+const checkPalindrome = (str) => {
     reverse = "" // empty string for storage
     
     for(let i = str.length - 1; i >= 0; i--) {
         reverse += str[i] }
-        if( reverse === str){
-            return true
-        } else { 
-            return false
-        }
+        // if( reverse === str){
+        //     return true
+        // } else { 
+        //     return false
+        // }
+        return reverse === str;
 
     }
-)
+
 
 console.log(checkPalindrome('121')); // true
 console.log(checkPalindrome('boob')); // true
