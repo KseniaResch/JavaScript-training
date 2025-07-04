b64682a9d181a2904291232fd017c56b46967d2e
KseniaResch <kseniaresch@gmail.com>
2025-07-04 16:32:30 +0200

    erster commit

--- funnyCircles/index.js
+++ funnyCircles/index.js
@@ -2,24 +2,40 @@
 //1.function to generate a circle
 //2. write function for unique color
 //3. 
+
+const amountOfcirclesArray = 5 ;
+
 const container = document.getElementById("container")
 document.addEventListener("mousemove", (event123)=> {
     console.log("mousemove", event123)
 })
 
-document.addEventListener("mousedown", (event)=> {
+ 
+
+
+const click = () => {document.addEventListener("mousedown", (event)=> {
     console.log("mousedown", event)
 })
-document.addEventListener("mouseup", (event)=> {
+ document.addEventListener("mouseup", (event)=> {
     console.log("mouseup", event)
-})
+})}
+
+const doubleClick 
+
+
 const createCircle = () => {
    const element = document.createElement("div")
    element.className = "circle";
    element.style.backgroundColor = "blue";
    container.appendChild(element)
 }
-createCircle();
-createCircle();
-createCircle();
-createCircle();
+
+const drawCircles = (amountOfcircles) => {
+     if (doubleClick){
+    for(let i = 0; i <= amountOfcircles; i++){
+        createCircle()
+    }
+     }
+}
+
+drawCircles();
